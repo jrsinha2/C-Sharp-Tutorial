8594bd90a77297a8a08aa492df980f8f4ad6272b
Aditya Sinha <31968928+jrsinha2@users.noreply.github.com>
2019-03-07 00:41:40 +0530

    Add files via upload

---
+++ branches-tutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =0;i<2;i++)
+            {   int j =0;
+                while(j<4)
+                {
+                    Console.WriteLine($"{j}");
+                    j++;
+                }
+            }
+        }
+    }
+}
